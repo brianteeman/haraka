004cf311230e573dffc25d20a361f1eff99e6c1b
zero-24 <zero-24@users.noreply.github.com>
2016-12-23 15:21:04 +0100

    initial cs review as Christmas gift

--- fields/whitelist.php
+++ fields/whitelist.php
@@ -4,24 +4,42 @@
  * @author Brian Teeman
  * @copyright (C) 2016 - Brian Teeman
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
-**/
+ */
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined( '_JEXEC' ) or die;
 
 JFormHelper::loadFieldClass('textarea');
 
+/**
+ * Provides a Whitelist
+ *
+ * @since  1.0.0
+ */
 class JFormFieldWhitelist extends JFormFieldTextarea
 {
-
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.0.0
+	 */
 	protected $type = 'Whitelist';
 
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   1.0.0
+	 */
 	protected function getInput()
 	{
-		$button = '<br />';
-		$onclick = "jQuery('#jform_params_".$this->element['name']."').val(jQuery('#jform_params_".$this->element['name']."').val()+(jQuery('#jform_params_".$this->element['name']."').val() ? '\\n' : '')+'".$_SERVER['REMOTE_ADDR']."'); return false;";
-		$button.= '<button class="btn" onclick="'.$onclick.'" href="#">'.JText::_('PLG_SYSTEM_HARAKA_CFG_WHITELIST_ADD_CURRENT').'</button>';
-		$onclick = "jQuery('#jform_params_".$this->element['name']."').val(''); return false;";
-		$button.= ' <button class="btn" onclick="'.$onclick.'" href="#">'.JText::_('PLG_SYSTEM_HARAKA_CFG_WHITELIST_CLEAR').'</button>';
-		return parent::getInput().$button;
+		$button  = '<br />';
+		$onclick = "jQuery('#jform_params_" . $this->element['name'] . "').val(jQuery('#jform_params_" . $this->element['name'] . "').val()+(jQuery('#jform_params_" . $this->element['name']."').val() ? '\\n' : '')+'" . $_SERVER['REMOTE_ADDR'] . "'); return false;";
+		$button .= '<button class="btn" onclick="' . $onclick . '" href="#">' . JText::_('PLG_SYSTEM_HARAKA_CFG_WHITELIST_ADD_CURRENT') . '</button>';
+		$onclick = "jQuery('#jform_params_" . $this->element['name']."').val(''); return false;";
+		$button .= ' <button class="btn" onclick="' . $onclick . '" href="#">' . JText::_('PLG_SYSTEM_HARAKA_CFG_WHITELIST_CLEAR') . '</button>';
+
+		return parent::getInput() . $button;
 	}
 }
