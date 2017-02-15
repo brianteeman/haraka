1c3663233684e70937e714500873ebdf62fdfa03
zero-24 <zero-24@users.noreply.github.com>
2017-02-15 13:17:02 +0100

    get the URl vars back as requested.

--- haraka.php
+++ haraka.php
@@ -80,14 +80,14 @@ class plgSystemHaraka extends JPlugin
 
 		if (!$this->displayHaraka)
 		{
-			$bgimageUrl    = JUri::base() . 'media/plg_haraka/images/' . $this->params->get('bgimage', 'comingsoon.jpg');
-			$caption       = $this->params->get('caption', '');
-			$countdown     = $this->params->get('countdown', 1);
-			$countdownDate = $this->params->get('countdown_date', '');
-			$font          = explode('|', $this->params->get('fonts', 'Roboto+Slab|Roboto'));
-			$fontcss       = str_replace('+',' ', $font);
-			$text          = $this->params->get('text', '<p>' . JText::_('PLG_SYSTEM_HARAKA_COMING_SOON') . '</p>');
-			$theme         = $this->params->get('theme', 'light');
+			$bgimageUrl     = JUri::base() . 'media/plg_haraka/images/' . $this->params->get('bgimage', 'comingsoon.jpg');
+			$caption        = $this->params->get('caption', '');
+			$countdown      = $this->params->get('countdown', 1);
+			$countdown_date = $this->params->get('countdown_date', '');
+			$font           = explode('|', $this->params->get('fonts', 'Roboto+Slab|Roboto'));
+			$fontcss        = str_replace('+',' ', $font);
+			$text           = $this->params->get('text', '<p>' . JText::_('PLG_SYSTEM_HARAKA_COMING_SOON') . '</p>');
+			$theme          = $this->params->get('theme', 'light');
 
 			// Meta
 			$metaDesc = $this->params->get('meta_desc', $this->app->get('MetaDesc'));
@@ -101,6 +101,12 @@ class plgSystemHaraka extends JPlugin
 			$twitter   = $this->params->get('twitter', '');
 			$youtube   = $this->params->get('youtube', '');
 
+			// Social Media URL's
+			$facebook_url  = 'https://facebook.com/' . $facebook;
+			$instagram_url = 'https://twitter.com/' . $instagram;
+			$twitte_url    = 'https://instagram.com/' . $twitter;
+			$youtube_url   = 'https://youtube.com/' . $youtube;
+			
 			$path = JPluginHelper::getLayoutPath('system', 'haraka');
 			include $path;
 
